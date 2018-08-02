6996d08aea4db30b2799263f4d53f8358f669abf
Molly Trombley-McCann <molly.kayline@gmail.com>
2018-08-02 13:26:09 -0700

    Trip width and position based on time

--- src/Trip.js
+++ src/Trip.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 
 class Trip extends Component {
 	render() {
+		const duration = this.props.trip.endTime - this.props.trip.startTime
+		
 		return(
-			<div className="trip">{this.props.trip.id}</div>
+			<div 
+				className="trip" 
+				style={{ left: `${this.props.trip.startTime}px`, width: `${duration}px` }}
+			>
+				{this.props.trip.id}
+			</div>
 		)
 	}
 }
